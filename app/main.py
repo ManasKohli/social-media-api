b3cd95b162c1323e60d17f374e2914b6126e37bf
ManasKohli <manaskohli6@gmail.com>
2025-12-29 18:31:47 -0500

    feat: add upload and view post endpoints

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_async_engine, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from contextlib import asynccontextmanager
 
 @asynccontextmanager
@@ -11,6 +12,41 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+# Endpoint to upload a post
+@app.post("/upload")
+async def upload_post(
+    caption: str = Form(...),
+    file: UploadFile = File(...),
+    session: AsyncSession = Depends(get_async_session)
+):
+    post = Post(
+        caption=caption,
+        url="dummy url",
+        file_type= "photo",
+        file_name= 'dummy name'
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post 
+
+#Feed
+@app.get("/feed")
+async def get_feed(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    post_data = []
+    for post in posts:
+        post_data.append({
+            "id": str(post.id),
+            "caption": post.caption,
+            "url": post.url,
+            "file_type": post.file_type,
+            "file_name": post.file_name,
+            "created_at": post.created_at.isoformat()
+        })  
+    return {"posts": post_data}
 
 #health check endpoint
 @app.get('/')
